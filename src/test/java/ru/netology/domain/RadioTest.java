ef6a56882f9facf36f41d2cfa25955e5c051ff4a
Anna Belousova <annabelousovva@gmail.com>
2020-09-17 20:22:47 +0300

    Edit pom.xml and RadioTest

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -9,79 +9,79 @@ class RadioTest {
     Radio radio = new Radio(4, 0, 10, 6, 0, 100);
 
     @Test
-    public void shouldGetMaxStation() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldGetMaxStation() {
         radio.getMaxStationNumber();
         assertEquals(10, radio.maxStationNumber);
     }
 
     @Test
-    public void shouldGetMinStation() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldGetMinStation() {
         radio.getMinStationNumber();
         assertEquals(0, radio.minStationNumber);
     }
 
     @Test
-    public void shouldGetCurrentStation() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldGetCurrentStation() {
         radio.getCurrentStationNumber();
         assertEquals(4, radio.currentStationNumber);
     }
 
     @Test
-    public void shouldGetMaxVolume() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldGetMaxVolume() {
         radio.getMaxVolume();
         assertEquals(100, radio.maxVolume);
     }
 
     @Test
-    public void shouldGetMinVolume() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldGetMinVolume() {
         radio.getMinVolume();
         assertEquals(0, radio.minVolume);
     }
 
     @Test
-    public void shouldGetCurrentVolume() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldGetCurrentVolume() {
         radio.getCurrentVolume();
         assertEquals(6, radio.currentVolume);
     }
 
     @Test
-    public void shouldSetMaxStation() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldSetMaxStation() {
         radio.setMaxStationNumber(25);
         assertEquals(25, radio.maxStationNumber);
     }
 
     @Test
-    public void shouldSetMinStation() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldSetMinStation() {
         radio.setMinStationNumber(2);
         assertEquals(2, radio.minStationNumber);
     }
 
     @Test
-    public void shouldSetCurrentStation() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldSetCurrentStation() {
         radio.setCurrentStationNumber(8);
         assertEquals(8, radio.currentStationNumber);
     }
 
     @Test
-    public void shouldSetMaxVolume() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldSetMaxVolume() {
         radio.setMaxVolume(200);
         assertEquals(200, radio.maxVolume);
     }
 
     @Test
-    public void shouldSetMinVolume() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldSetMinVolume() {
         radio.setMinVolume(11);
         assertEquals(11, radio.minVolume);
     }
 
     @Test
-    public void shouldSetCurrentVolume() {  //Можно задавать количество радиостанций при создании объекта (по умолчанию - 10)
+    public void shouldSetCurrentVolume() {
         radio.setCurrentVolume(99);
         assertEquals(99, radio.currentVolume);
     }
 
     @Test
-    public void shouldIncreaseCurrentStation() {   //Номер текущей радиостанции изменяется в пределах от 0 до количества радиостанций, указанных при создании (см. п.1)
+    public void shouldIncreaseCurrentStation() {
         radio.setCurrentStationNumber(4);
         System.out.println("StationNumber before increase is " + radio.currentStationNumber);
         radio.increaseStationNumber();
@@ -90,7 +90,7 @@ class RadioTest {
     }
 
     @Test
-    public void shouldDecreaseCurrentStation() {  //Номер текущей радиостанции изменяется в пределах от 0 до количества радиостанций, указанных при создании (см. п.1)
+    public void shouldDecreaseCurrentStation() {
         radio.setCurrentStationNumber(3);
         System.out.println("StationNumber before decrease is " + radio.currentStationNumber);
         radio.decreaseStationNumber();
@@ -99,7 +99,7 @@ class RadioTest {
     }
 
     @Test
-    public void shouldReloadCurrentStationToMin() {  //Если текущая радиостанция - максимальная, и клиент нажал на кнопку next (следующая) на пульте, то текущей должна стать 0-ая
+    public void shouldReloadCurrentStationToMin() {
         radio.setCurrentStationNumber(10);
         System.out.println("StationNumber before increase is " + radio.currentStationNumber);
         radio.increaseStationNumber();
@@ -109,7 +109,7 @@ class RadioTest {
     }
 
     @Test
-    public void shouldReloadCurrentStationToMax() {  //Если текущая радиостанция - 0, и клиент нажал на кнопку prev (предыдущая) на пульте, то текущей должна стать максимальная
+    public void shouldReloadCurrentStationToMax() {
         radio.setCurrentStationNumber(0);
         System.out.println("StationNumber before increase is " + radio.currentStationNumber);
         radio.decreaseStationNumber();
@@ -117,71 +117,46 @@ class RadioTest {
         assertEquals(10, radio.currentStationNumber);
     }
 
-
     @Test
-    public void shouldIncreaseCurrentVolume() {   //Клиент должен иметь возможность увеличивать уровень громкости звука (в пределах от 0 до 100)
+    public void shouldIncreaseCurrentVolume() {
         radio.setCurrentVolume(6);
         System.out.println("Volume before increase is " + radio.currentVolume);
         radio.increaseVolume();
         System.out.println("Volume after increase is " + radio.currentVolume);
         assertEquals(7, radio.currentVolume);
-
     }
 
     @Test
-    public void shouldDecreaseCurrentVolume() {         //Клиент должен иметь возможность уменьшать уровень громкости звука (в пределах от 0 до 100)
+    public void shouldDecreaseCurrentVolume() {
         radio.setCurrentVolume(6);
         System.out.println("Volume before decrease is " + radio.currentVolume);
         radio.decreaseVolume();
         System.out.println("Volume after decrease is " + radio.currentVolume);
         assertEquals(5, radio.currentVolume);
-
     }
 
     @Test
-    public void shouldNotReloadVolumeMax() {        //Если уровень громкости звука достиг максимального значения, то дальнейшее нажатие на + не должно ни к чему приводить
+    public void shouldNotReloadVolumeMax() {
         radio.setCurrentVolume(100);
         System.out.println("Volume before increase is " + radio.currentVolume);
         radio.increaseVolume();
         System.out.println("Volume after increase is " + radio.currentVolume);
         assertEquals(100, radio.currentVolume);
-
     }
 
     @Test
-    public void shouldNotReloadVolumeMin() {        //Если уровень громкости звука достиг минимального значения, то дальнейшее нажатие на - не должно ни к чему приводить
+    public void shouldNotReloadVolumeMin() {
         radio.setCurrentVolume(0);
         System.out.println("Volume before decrease is " + radio.currentVolume);
         radio.decreaseVolume();
         System.out.println("Volume after decrease is " + radio.currentVolume);
         assertEquals(0, radio.currentVolume);
-
     }
 
     @Test
-    public void testToString() {
-        String expected = "Radio(currentStationNumber=4, minStationNumber=0, maxStationNumber=10, currentVolume=6, minVolume=0, maxVolume=100)";
-        assertEquals(expected, radio.toString());
+    public void testDefaultConstructor() {   //тест на проверку дефолтного коструктора
+        Radio radio1 = new Radio();
+        radio1.setCurrentVolume(50);
+        assertEquals(50, radio1.currentVolume);
     }
-
-    @Test
-    public void testCanEqual (){
-       boolean expected = false;
-       assertEquals(expected, radio.canEqual(radio.currentStationNumber));
-    }
-
-    @Test
-    public void sholdTestEqual (){
-        boolean expected = radio.equals(5);
-        assertEquals(false, radio.canEqual(radio.currentStationNumber));
-    }
-
-    @Test
-    public void testDefaultConstructor (){
-        Radio radio = new Radio ();
-        radio.setCurrentVolume(50);
-        assertEquals(50, radio.currentVolume);
-
-    }
-
 }
